070d5b0fc562d1d1460cf1075113fe693c8fe4b5
Max Base <maxbasecode@gmail.com>
2021-10-30 16:49:55 +0330

    add deleteTodo

--- script.js
+++ script.js
@@ -151,6 +151,10 @@ class TodoItem {
 	oncreate() {
 
 	}
+	deleteTodo() {
+		console.log(`delete ${this.todo.index}`)
+		globalState.todos.splice(this.todo.index, 1)
+	}
 	view() {
 		return m("div", {class:"todo"},
 			[
@@ -169,7 +173,8 @@ class TodoItem {
 								m("path", {"d":"M5,21h14c1.103,0,2-0.897,2-2v-8.668l-2,2V19H8.158c-0.026,0-0.053,0.01-0.079,0.01c-0.033,0-0.066-0.009-0.1-0.01H5V5\th6.847l2-2H5C3.897,3,3,3.897,3,5v14C3,20.103,3.897,21,5,21z"})
 							]
 						),
-						m("svg", {class:"settings__icon", "stroke":"currentColor", "fill":"currentColor", "stroke-width":"0", "viewBox":"0 0 24 24", "data-action":"delete", "height":"1em", "width":"1em", "xmlns":"http://www.w3.org/2000/svg"}, 
+						m("svg", {class:"settings__icon", "stroke":"currentColor", "fill":"currentColor", "stroke-width":"0", "viewBox":"0 0 24 24", "data-action":"delete", "height":"1em", "width":"1em", "xmlns":"http://www.w3.org/2000/svg",
+							onclick: this.deleteTodo.bind(this)}, 
 							m("path", {"d":"M15,2H9C7.897,2,7,2.897,7,4v2H3v2h2v12c0,1.103,0.897,2,2,2h10c1.103,0,2-0.897,2-2V8h2V6h-4V4C17,2.897,16.103,2,15,2z M9,4h6v2H9V4z M17,20H7V8h1h8h1V20z"})
 						)
 					]
@@ -202,8 +207,13 @@ class MainComponent {
 									]
 								),
 							]
-							: globalState.todos.map((todo) => {
-								return m(TodoItem, {todo: todo})
+							: globalState.todos.map((todo, index) => {
+								return m(TodoItem, {
+									todo: {
+										index: index,
+										...todo
+									}
+								})
 							})
 					)
 				)
