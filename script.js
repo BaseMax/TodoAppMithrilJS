d0a7165460f3c46e3f19a0baf7429984f10d6062
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-01-27 18:04:51 +0100

    added anonymous onClick-Function to the svg so that it toggles between dark and light theme

--- script.js
+++ script.js
@@ -3,7 +3,43 @@ const globalState = {
 	editing: undefined,
 	todoInput: "",
 	todoCategory: "RED",
+	/*
+		The further values are used for the implementation of the Dark-Light-Mode-Toggling.
+		The properties can be changed. Due to that, you can use your wished colors instead of white and black.
+	*/
+	lightMode: true;
+	lightTheme: {
+		color: "white",
+		textColor: "black"
+		//Placeholder for the path of the sun-svg:
+		buttonImage: ""
+	},
+	darkTheme: {
+		color: "black",
+		textColor: "white"
+		//Placeholder for the path of the moon-svg:
+		buttonImage: ""
+	}
 }
+
+
+
+
+/*
+function toggleLightAndDarkMode() {
+	let color = ""
+	if (globalState.lightMode) {
+		color = "black";
+		globalState.lightMode = false;
+	} else {
+		color = "white";
+		globalState.lightMode = true;
+	}
+	
+	//document.getElementsByClassName('app').style.backgroundColor = color;
+	console.log(`${color}`);
+}
+*/
 
 const globalAction = {
 	changeTodo: (value) => {
@@ -44,7 +80,36 @@ class HeaderComponent {
 	view() {
 		return m("header", {class:"header", style:{"border-bottom-color":"var(--textColor)"}},
 			[
-				m("svg", {class:"header__icon header__icon--mode", "stroke":"currentColor", "fill":"currentColor", "stroke-width":"0", "viewBox":"0 0 24 24", "height":"1em", "width":"1em", "xmlns":"http://www.w3.org/2000/svg"}, 
+				m("svg", {class:"header__icon header__icon--mode", id:"switchModeButton", "stroke":"currentColor", "fill":"currentColor", "stroke-width":"0", "viewBox":"0 0 24 24", "height":"1em", "width":"1em", "xmlns":"http://www.w3.org/2000/svg",
+							onclick: () => {
+									let color = ""
+
+									//switching the Mode from light to dark and vice versa
+									if (globalState.lightMode) {
+										color = globalState.darkTheme.color;
+										globalState.lightMode = false;
+									} else {
+										color = globalState.lightTheme.color;
+										globalState.lightMode = true;
+									}
+									
+									//configuring the style-properties in order to the actual theme
+									let container = document.getElementById('container');
+									container.style.backgroundColor = color;
+									/*
+									Placeholder for further toggling of HTML-elements:
+
+									//toggling the svg
+									let toggleSVG = document.getElementById("switchModeButton");
+
+
+									//toggling the text:
+
+
+									*/
+								
+							}
+						}, 
 					m("path", {"d":"M20.742,13.045c-0.677,0.18-1.376,0.271-2.077,0.271c-2.135,0-4.14-0.83-5.646-2.336c-2.008-2.008-2.799-4.967-2.064-7.723 c0.092-0.345-0.007-0.713-0.259-0.965C10.444,2.04,10.077,1.938,9.73,2.034C8.028,2.489,6.476,3.382,5.241,4.616 c-3.898,3.898-3.898,10.243,0,14.143c1.889,1.889,4.401,2.93,7.072,2.93c2.671,0,5.182-1.04,7.07-2.929 c1.236-1.237,2.13-2.791,2.583-4.491c0.092-0.345-0.008-0.713-0.26-0.965C21.454,13.051,21.085,12.951,20.742,13.045z M17.97,17.346c-1.511,1.511-3.52,2.343-5.656,2.343c-2.137,0-4.146-0.833-5.658-2.344c-3.118-3.119-3.118-8.195,0-11.314 c0.602-0.602,1.298-1.102,2.06-1.483c-0.222,2.885,0.814,5.772,2.89,7.848c2.068,2.069,4.927,3.12,7.848,2.891 C19.072,16.046,18.571,16.743,17.97,17.346z"})
 				),
 				m("h1", {class:"header__logo"}, 
@@ -294,7 +359,7 @@ class ThemeComponent {
 
 class ContainerComponent {
 	view({children}) {
-		return m("div", {class:"container"},
+		return m("div", {class:"container", id:"container"},
 			children
 		)
 	}
