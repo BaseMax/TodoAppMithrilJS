86c9c5f05183f40ed45336f52c95d050c3b2cf71
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-01-27 18:06:57 +0100

    fixed a syntax-bug

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ const globalState = {
 		The further values are used for the implementation of the Dark-Light-Mode-Toggling.
 		The properties can be changed. Due to that, you can use your wished colors instead of white and black.
 	*/
-	lightMode: true;
+	lightMode: true,
 	lightTheme: {
 		color: "white",
-		textColor: "black"
+		textColor: "black",
 		//Placeholder for the path of the sun-svg:
 		buttonImage: ""
 	},
 	darkTheme: {
 		color: "black",
-		textColor: "white"
+		textColor: "white",
 		//Placeholder for the path of the moon-svg:
 		buttonImage: ""
 	}
